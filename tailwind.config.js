c76ad4cdec2cdb39c9b7a8968d9303abab994b1c
Brandon <bkw1491@gmail.com>
2022-01-21 14:02:04 +0000

    add colors to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        accent_1: "#00aea5",
+        accent_1_hover: "#a4fcf8",
+        accent_2: "#eaade5",
+        accent_2_hover: "#e6daef"
+      },
+    },
   },
-  plugins: [],
 }
