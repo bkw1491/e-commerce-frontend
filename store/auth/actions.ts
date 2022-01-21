c91975138eda1ae231f1a361934cb00bc6f658bf
Brandon <bkw1491@gmail.com>
2022-01-21 17:45:33 +0000

    clean up

--- store/auth/actions.ts
+++ store/auth/actions.ts
@@ -1,11 +1,11 @@
 import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
-import { register, login } from '@api/user.api';
+import { UserAPI } from '@api/user.api';
 import { IUser } from '@interfaces/IUser';
 
 export const userRegister = createAsyncThunk(
   'auth/register',
   async (creds: Omit<IUser, "id">) => {
-    const { data, error } = await register(creds);
+    const { data, error } = await UserAPI.register(creds);
     if(error) { throw new Error(error) } //rejects promise on thunk
     return data;
   }
@@ -14,7 +14,7 @@ export const userRegister = createAsyncThunk(
 export const userLogin = createAsyncThunk(
   'auth/login',
   async (creds: Omit<IUser, "id" | "confirm">) => {
-    const { data, error } = await login(creds);
+    const { data, error } = await UserAPI.login(creds);
     if(error) { throw new Error(error) } //rejects promise on thunk
     return data;
   }
