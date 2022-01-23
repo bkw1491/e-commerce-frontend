b98d06896688bf739326e29e76bc8a7c744e2128
Brandon <bkw1491@gmail.com>
2022-01-23 15:14:01 +0000

    font change

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         accent_1: "#00aea5",
