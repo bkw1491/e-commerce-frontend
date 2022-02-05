7ddd320ed199bbaa744b0ebb80f4dbc47396d304
Brandon <bkw1491@gmail.com>
2022-02-05 17:56:41 +0000

    fix issue where state wasn't updating as expected after login or register

--- pages/user/login.tsx
+++ pages/user/login.tsx
@@ -1,43 +1,35 @@
-import Form from '@components/Form';
-import Router from 'next/router';
-import React from 'react';
+import Form from "@components/Form";
+import Router from "next/router";
+import React from "react";
 
-import { useAppDispatch } from '@hooks/useAppDispatch';
-import { useAppSelector } from '@hooks/useAppSelector';
-import { userLogin } from '@store/auth/actions';
-import { IUser } from '@interfaces/IUser';
-import { cartGet } from '@store/cart/actions';
-
+import { useEffect } from "react";
+import { useAppDispatch } from "@hooks/useAppDispatch";
+import { useAppSelector } from "@hooks/useAppSelector";
+import { userLogin } from "@store/auth/actions";
+import { IUser } from "@interfaces/IUser";
 
 export default function Login() {
-  //bring in pre-typed custom dispatch and selector hooks
-  const { error } = useAppSelector(state => state.auth);
-  const dispatch = useAppDispatch();
+	//bring in pre-typed custom dispatch and selector hooks
+	const { error, authed } = useAppSelector(state => state.auth);
+	const dispatch = useAppDispatch();
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    //stops page refeshing on submit
-    e.preventDefault();
-    //constructs set of key value pairs from form fields
-    const form = new FormData(e.currentTarget);
-    //convert key-value pairs into obj
-    const creds = Object.fromEntries(form.entries()) as 
-      Omit<IUser, "id" | "confirm">;
-    //event triggered by dispatch contains call to api
-    //must await so have auth for getCart below
-    await dispatch(userLogin(creds));
-    //if success get the user's cart
-    if(!error) {
-      dispatch(cartGet());
-      //navigate to home page
-      Router.push('/');
-    }
-  }
+	useEffect(() => {
+		if (authed) Router.push("/");
+	}, [authed]);
 
-  return (
+	async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		//stops page refeshing on submit
+		e.preventDefault();
+		//constructs set of key value pairs from form fields
+		const form = new FormData(e.currentTarget);
+		//convert key-value pairs into obj
+		const creds = Object.fromEntries(form.entries()) as Omit<
+			IUser,
+			"id" | "confirm"
+		>;
 
-    <Form
-      type={"login"}
-      error={error}
-      submit={handleSubmit}/>
-  )
+		dispatch(userLogin(creds));
+	}
+
+	return <Form type={"login"} error={error} submit={handleSubmit} />;
 }
--- pages/user/register.tsx
+++ pages/user/register.tsx
@@ -1,38 +1,32 @@
-import React from 'react';
-import Router from 'next/router';
-import Form from '@components/Form';
+import React from "react";
+import Router from "next/router";
+import Form from "@components/Form";
 
-import { useAppDispatch } from '@hooks/useAppDispatch';
-import { userRegister } from '@store/auth/actions';
-import { IUser } from '@interfaces/IUser';
-import { useAppSelector } from '@hooks/useAppSelector';
-
+import { useEffect } from "react";
+import { useAppDispatch } from "@hooks/useAppDispatch";
+import { userRegister } from "@store/auth/actions";
+import { IUser } from "@interfaces/IUser";
+import { useAppSelector } from "@hooks/useAppSelector";
 
 export default function Register() {
-  //bring in pre-typed custom dispatch and selector hooks
-  const { error } = useAppSelector(state => state.auth);
-  const dispatch = useAppDispatch();
+	//bring in pre-typed custom dispatch and selector hooks
+	const { error, authed } = useAppSelector(state => state.auth);
+	const dispatch = useAppDispatch();
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    //stops page refeshing on submit
-    e.preventDefault();
-    //constructs set of key value pairs from form fields
-    const form = new FormData(e.currentTarget);
-    //convert key-value pairs into obj
-    const creds = Object.fromEntries(form.entries()) as 
-      Omit<IUser, "id">;
-    //event triggered by dispatch contains call to api
-    dispatch(userRegister(creds));
-    //navigate to login page if success
-    if(!error) { Router.push('/user/login') }
-  }
+	useEffect(() => {
+		if (!authed) Router.push("/user/login");
+	}, [authed]);
 
-  return (
+	async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		//stops page refeshing on submit
+		e.preventDefault();
+		//constructs set of key value pairs from form fields
+		const form = new FormData(e.currentTarget);
+		//convert key-value pairs into obj
+		const creds = Object.fromEntries(form.entries()) as Omit<IUser, "id">;
+		//event triggered by dispatch contains call to api
+		dispatch(userRegister(creds));
+	}
 
-    <Form
-      type={"register"}
-      error={error}
-      submit={handleSubmit}/>
-
-  )
+	return <Form type={"register"} error={error} submit={handleSubmit} />;
 }
