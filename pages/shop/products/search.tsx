9681ecd80e0c7614bd70f9654295e2fe401675e3
Brandon <bkw1491@gmail.com>
2022-02-06 15:02:36 +0000

    simple product search page

---
+++ pages/shop/products/search.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import ProductCard from "@components/ProductCard";
+
+import { useState } from "react";
+import { SearchIcon } from "@heroicons/react/solid";
+import { ShopAPI } from "@api/shop";
+import { IProduct } from "@interfaces/IProduct";
+import { useDebounce } from "@hooks/useDebounce";
+
+export default function ProductsByName() {
+	const [results, setResults] = useState([] as IProduct[]);
+
+	const handleSearch = useDebounce<React.ChangeEvent<HTMLInputElement>>(
+		async e => {
+			const search = e.target.value;
+			if (search.length < 3) {
+				setResults([]);
+				return;
+			}
+			const { data, error } = await ShopAPI.searchByName(search);
+			if (!error) setResults(data);
+		},
+		400
+	);
+
+	return (
+		<div className="bg-darkest relative top-24 w-full lg:px-6 xl:px-8">
+			<form className="text-contrast flex h-24 w-full lg:h-44">
+				<input
+					name="search"
+					type="text"
+					onChange={handleSearch}
+					placeholder="Search by Typing Keywords..."
+					className="focus:ring-accent_1_hover focus:border-accent bg-darkest text-contrast relative mb-2 block w-full appearance-none px-5 placeholder-gray-500 focus:outline-none sm:text-sm"
+				/>
+			</form>
+
+			{results.length > 0 ? (
+				<section className="grid grid-cols-2 gap-2 px-1 md:grid-cols-3 md:px-5 lg:grid-cols-5 lg:gap-6 xl:grid-cols-6 xl:gap-10">
+					{results.map(product => {
+						return <ProductCard key={product.id} product={product} />;
+					})}
+				</section>
+			) : (
+				<h2 className="text-contrast text-center text-2xl">
+					No Products Found
+				</h2>
+			)}
+		</div>
+	);
+}
